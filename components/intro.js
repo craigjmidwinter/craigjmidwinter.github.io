150b12248ebf8eaa5e27c1c3fd5d5a1158602445
Craig Midwinter <craig.j.midwinter@gmail.com>
2021-05-12 21:52:36 -0500

    Purge readme and add contact info

--- components/intro.js
+++ components/intro.js
@@ -27,8 +27,15 @@ const IntroBody = styled.div`
   font-family: CabinVariable;
   font-size: 1.25rem;
 `;
+const Contact = styled.div`
+  font-family: CabinVariable;
+    margin: 0.5rem;
+  margin-bottom: 1rem;
+`
 const HeaderText = styled.h1`
   font-size: 4rem;
+  margin-top: 1rem;
+  margin-bottom: 2rem;
 `;
 
 export default function Intro() {
@@ -39,10 +46,14 @@ export default function Intro() {
       </AvatarContainer>
       <IntroTextContainer>
         <HeaderText>Craig J. Midwinter</HeaderText>
+          <Contact>
+              craig.j.midwinter@gmail.com<br/>
+              <a target="_blank" href="https://www.linkedin.com/in/craig-midwinter-b26193155/">linked.in</a>
+          </Contact>
         <IntroBody>
-          Experienced Developer who specializes in quickly adapting to new
-          languages and tools, and developing solutions in challenging problem
-          domains
+            Experienced Developer who specializes in quickly adapting to new
+            languages and tools, and developing solutions in challenging problem
+            domains.
         </IntroBody>
       </IntroTextContainer>
     </IntroHeader>
