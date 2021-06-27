b2aa306ebb9247ff4a1971c7a6d62c5f14365a50
craigjmidwinter <craig.j.midwinter@gmail.com>
2021-06-27 02:34:27 +0000

    Deploying to gh-pages from @ craigjmidwinter/craigjmidwinter.github.io@3ffd74be2f75a7bb977e80d938a8a79de61d5e9a 🚀

--- _next/static/lrbe2w4Su_mL9bzCiVWzs/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-20ed88f38d64afa55633.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-bd5f557696b53341da53.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
