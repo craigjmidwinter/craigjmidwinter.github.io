b2175d361be3dacec3dacdbd35aefc057bcd9f9a
Craig Midwinter <craig.j.midwinter@gmail.com>
2025-02-06 14:21:28 -0600

    DESTROYYYY

--- README.md
+++
@@ -1,3 +0,0 @@
-# Craig J. Midwinter's Resume
-
-Codebase for my resume static site. Built with Next.JS
--- src/themes/colors.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  purple: '#23263A',
-  DarkPurple: '#1F2235',
-  white: '#fff',
-  grey0: '#EBEBEB',
-  grey1: '#707070',
-  grey2: '#757575',
-}
