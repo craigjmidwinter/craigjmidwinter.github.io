01869cafc43885bd53e3904c25adda7874032735
Craig Midwinter <craig.j.midwinter@gmail.com>
2025-03-19 22:38:06 -0500

    Update Next.js configuration with asset prefix, base path, and trailing slash settings

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    output: 'export',
-    distDir: 'dist',
-    images: {unoptimized: true}
+  output: "export",
+  distDir: "dist",
+  images: { unoptimized: true },
+  assetPrefix: "/craigjmidwinter.github.io/",
+  basePath: "/craigjmidwinter.github.io",
+  trailingSlash: true,
 };
 
 export default nextConfig;
